7ad085ed2e44dd1f7ef4a72bcde74dfeb54fca64
Ammar <ammar.kusow2@gmail.com>
2022-01-20 16:08:13 -0500

    intial commit

---
+++ src/sidebar/SideBar.jsx
@@ -0,0 +1,23 @@
+import './sidebar.css'
+
+export default function SideBar() {
+  return <div className='sidebar'>
+    <div className="sidebarItem">
+      <span className="sidebartitle">About Me</span>
+      <img src="https://cdn.pixabay.com/photo/2020/02/13/06/49/seascape-4844697__480.jpg" alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur quis id rem accusamus adipisci assumenda odio voluptas. Fuga, laboriosam eos? Officiis rerum reprehenderit expedita sunt totam autem beatae provident.</p>
+    </div>
+    <div className="sidebarItem">
+    <span className="sidebartitle">Categories</span>
+    <ul className="sidebarlist">  
+    <li className="sidebarlistitem">Bitcoin</li>
+    <li className="sidebarlistitem">Doggo Coin</li>
+    <li className="sidebarlistitem">Etheruim</li>
+    <li className="sidebarlistitem">some coin</li>
+    </ul>
+    </div>
+    <div className="sidebarItem">
+    <span className="sidebartitle">Follow us</span>
+    </div>
+  </div>;
+}
