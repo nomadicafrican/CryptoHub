1182caff0063abc33cc20e68eb9b627989795441
Abdullahi Sabriye <abdullahisabriye@gmail.com>
2022-01-21 09:33:51 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavBar from "./components/navBar/navbar";
 import Home from "./pages/Home/Home";
 import SinglePage from "./pages/Home/single/SinglePage";
+import Write from "./pages/Home/write/write";
 
 function App() {
   return (
     <>
       <NavBar />
-      <SinglePage />;
+      <Write />;
     </>
   );
 }
---
+++ src/pages/Home/write/write.jsx
@@ -0,0 +1,27 @@
+import "./write.css"
+
+
+export default function Write () {
+  return (
+    <div classname = "write">
+     <form className="writeForm ">
+        <div className="writeFormGroup">
+
+          <input type="file" id="fileInput" />
+          <input type="text" placeholder="title" />
+
+        </div>
+
+
+
+     </form>
+
+
+    </div>
+
+
+
+
+
+  )
+}
