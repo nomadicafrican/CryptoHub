0cc4c26a00517b026c3628899d3ae428d93069e5
Ammar <ammar.kusow2@gmail.com>
2022-01-22 18:40:55 -0500

    added posts routes and queries

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose
   });
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen("3000", () => {
   console.log("backend is running");
