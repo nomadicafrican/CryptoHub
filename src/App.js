56bdba92013fc0940a5efbd838866d13826f2498
Abdullahi Sabriye <abdullahisabriye@gmail.com>
2022-01-22 02:54:36 -0500

    building react dom

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Write from "./pages/Home/write/write";
 import Settings from "./pages/Home/settings/settings";
 import Login from "./pages/Home/login/login";
 import Register from "./pages/Home/register/register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
+  const user = true;
   return (
-    <>
+    <Router>
       <NavBar />
-      <Register />;
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register"> {user ? <Home /> : <Register />} </Route>
+        <Route path="/login"> {user ? <Home /> : <Login />} </Route>
+        <Route path="/write"> {user ? <Write /> : <Register />} </Route>
+        <Route path="/settings"> {user ? <Settings /> : <Register />} </Route>
+        <Route path="/post/:postId">
+          <SinglePage />
+        </Route>
+      </Switch>
+
+
+
+
+    </Router>
   );
 }
 
