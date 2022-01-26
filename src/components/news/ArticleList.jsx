8a7c41ef8d337996b97c23db31c05b1f6480c311
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2022-01-26 09:35:29 -0500

    get api for news articles

---
+++ src/components/news/ArticleList.jsx
@@ -0,0 +1,16 @@
+// import Article from './Article'
+// function ArticleList() {
+//   console.log(props)
+//   // }
+//   const articleView = news?.map((article) => <Article key={article.id} articledata={article} name={article.title} 
+  
+//   />)
+  
+
+  
+//   return <div>
+//  {articleView}
+//   </div>;
+// }
+
+// export default ArticleList;
